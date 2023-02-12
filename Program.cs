17baf4ab15f32d1df8f882e6dc0d3554297d08c0
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-12 13:56:51 +0300

    Квадрат числа

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает
+// его квадрат (умноженное на само себя)
+System.Console.Write("Введите число > ");                                   // Вывод в консоль приглашения для ввода
+string inputValue;                                                          // Объявление переменной
+inputValue = Console.ReadLine();                                            // Присваивание переменной из консоли
+int value = Convert.ToInt32(inputValue);                                    // Ковертация в целое введенной строки
+
+int square = value * value;                                                 // Вычисление квадрата
+
+// Вывод на экран
+System.Console.WriteLine("Квадрат числа " + value + " равен " + square);
+System.Console.WriteLine($"Квадрат числа {value} равен {square}");
